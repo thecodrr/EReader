74308224df9303cc776196f327e9b49af8039e75
Brandon James <bjames105@outlook.com>
2017-01-27 12:36:40 -0500

    +Base model class for files in the library +DI Container class +MVVM Library installed +MVVM Dialog Library installed

---
+++ EReader/EReader/ViewModels/LibraryViewModel.cs
@@ -0,0 +1,81 @@
+﻿using EReader.Models;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MvvmDialogs;
+using MvvmDialogs.FrameworkPickers.FileOpen;
+using MvvmDialogs.FrameworkPickers.Folder;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+using Windows.Storage.Pickers;
+
+namespace EReader.ViewModels
+{
+    public class LibraryViewModel : ViewModelBase
+    {
+        private readonly MvvmDialogs.IDialogService dialogService;
+
+        private ObservableCollection<EReaderDocument> _ereaderDocuments;
+
+        public ObservableCollection<EReaderDocument> EReaderDocuments
+        {
+            get { return _ereaderDocuments; }
+            set { Set(ref _ereaderDocuments, value); }
+        }
+
+        public ICommand OpenFilePickerCommand { get; private set; }
+
+        public LibraryViewModel(IDialogService dialogService)
+        {
+            this.dialogService = dialogService;
+
+            InitData();
+
+            InitCommands();
+        }
+
+        private void InitData()
+        {
+            EReaderDocuments = new ObservableCollection<EReaderDocument>();
+        }
+
+        private void InitCommands()
+        {
+            OpenFilePickerCommand = new RelayCommand(OpenFilePickerAsync);
+        }
+
+        private async void OpenFilePickerAsync()
+        {
+            var settings = new FileOpenPickerSettings
+            {
+                SuggestedStartLocation = PickerLocationId.DocumentsLibrary,
+                FileTypeFilter = new List<string> { ".txt" }
+            };
+
+            IReadOnlyList<StorageFile> storageFiles = await dialogService.PickMultipleFilesAsync(settings);
+
+            if (storageFiles.Any())
+            {
+                foreach (var file in storageFiles)
+                {
+                    var token = StorageApplicationPermissions.FutureAccessList.Add(file);
+
+                    StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+                    StorageFile folderTokens = await storageFolder.CreateFileAsync("folderTokens.txt", CreationCollisionOption.ReplaceExisting);
+                    Debug.WriteLine(storageFolder.Path);
+
+                    EReaderDocuments.Add(new EReaderDocument(file));
+
+                    await FileIO.AppendTextAsync(folderTokens, token + ",");
+                }
+            }
+        }
+    }
+}
--- EReader/EReader/Views/LibraryView.xaml.cs
+++ EReader/EReader/Views/LibraryView.xaml.cs
@@ -26,33 +26,9 @@ namespace EReader.Views
     /// </summary>
     public sealed partial class LibraryView : Page
     {
-        internal List<EReaderDocument> EReaderDocuments { get; private set; }
-
         public LibraryView()
         {
             this.InitializeComponent();
-        }
-
-        private async void AddNewFileButton_ClickAsync(object sender, RoutedEventArgs e)
-        {
-            var picker = new Windows.Storage.Pickers.FileOpenPicker();
-            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
-            picker.SuggestedStartLocation =
-                Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
-
-            StorageFile file = await picker.PickSingleFileAsync();
-            if (file != null)
-            {
-                var token = StorageApplicationPermissions.FutureAccessList.Add(file);
-
-                StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
-                StorageFile folderTokens = await storageFolder.CreateFileAsync("folderTokens.txt", CreationCollisionOption.ReplaceExisting);
-                Debug.WriteLine(storageFolder.Path);
-
-                EReaderDocuments.Add(new EReaderDocument(file));
-
-                await FileIO.WriteTextAsync(folderTokens, token);
-            }
         }
     }
 }
