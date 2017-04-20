9571cad03cb56c0e1a78eee5e3121bb57bcf5585
theweavrs <abdullahatta31@gmail.com>
2017-04-21 00:31:02 +0500

    add smooth scrolling

--- EReader/EReader/Views/DocumentView.xaml.cs
+++ EReader/EReader/Views/DocumentView.xaml.cs
@@ -18,46 +18,47 @@ namespace EReader.Views
         }
         async void OpenFile()
         {
-            //FileOpenPicker picker = new FileOpenPicker();
-            //picker.FileTypeFilter.Add(".epub");
-            //var file = await picker.PickSingleFileAsync();
-            //if (file != null)
-            //{
-            //    Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.Add(file);
-            //    EReader.Epub.Engine engine = new Epub.Engine();
-            //    var book = await engine.ReadEpubAsync(file, false);
-            //    var uri = book.epubFile.ConstructApplicationUriFromStorageFile();
-            //    DocumentViewer.Navigate(uri);
-            //    GC.Collect();
-            //}
-            //DocumentViewer.NavigationStarting += DocumentViewer_NavigationStarting;
-
-            FolderPicker picker = new FolderPicker();
+            FileOpenPicker picker = new FileOpenPicker();
             picker.FileTypeFilter.Add(".epub");
-            var folder = await picker.PickSingleFolderAsync();
-            if (folder != null)
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
             {
-                foreach (var file in await folder.GetFilesAsync())
-                {
-                    if (file.FileType.Contains("epub"))
-                    {
-                        Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.Add(file);
-                        EReader.Epub.Engine engine = new Epub.Engine();
-                        var book = await engine.ReadEpubAsync(file, false);
-                        //var uri = book.epubFile.ConstructApplicationUriFromStorageFile();
-                        //DocumentViewer.Navigate(new Uri(uri));
-                    }
-                }
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.Add(file);
+                EReader.Epub.Engine engine = new Epub.Engine();
+                var book = await engine.ReadEpubAsync(file, false);
+                var uri = book.epubFile.ConstructApplicationUriFromStorageFile();
+                DocumentViewer.Navigate(uri);
+                GC.Collect();
             }
             DocumentViewer.NavigationStarting += DocumentViewer_NavigationStarting;
+
+            //FolderPicker picker = new FolderPicker();
+            //picker.FileTypeFilter.Add(".epub");
+            //var folder = await picker.PickSingleFolderAsync();
+            //if (folder != null)
+            //{
+            //    foreach (var file in await folder.GetFilesAsync())
+            //    {
+            //        if (file.FileType.Contains("epub"))
+            //        {
+            //            Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.Add(file);
+            //            EReader.Epub.Engine engine = new Epub.Engine();
+            //            var book = await engine.ReadEpubAsync(file, false);
+            //            //var uri = book.epubFile.ConstructApplicationUriFromStorageFile();
+            //            //DocumentViewer.Navigate(new Uri(uri));
+            //        }
+            //    }
+            //}
+            //DocumentViewer.NavigationStarting += DocumentViewer_NavigationStarting;
         }
 
 
         private async void DocumentViewer_NavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
         {
             args.Cancel = true;
+            string functionString = "function scrollTo(a,b,c){if(!(c<=0)){var d=b-a.scrollTop,e=d/c*10;setTimeout(function(){a.scrollTop=a.scrollTop+e,a.scrollTop!==b&&scrollTo(a,b,c-10)},10)}}";
            // string alternateScript = string.Format("var els = document.querySelectorAll(\"a[href~='{0}']\");var el = els[0];el.scrollIntoView();", args.Uri.Fragment.Replace("#", ""));
-            string functionString = String.Format("document.getElementById('{0}').scrollIntoView();", args.Uri.Fragment.Replace("#",""));
+            functionString += String.Format("elmnt = document.getElementById('{0}'); scrollTo(document.body, elmnt.offsetTop, 600);", args.Uri.Fragment.Replace("#",""));
             var res = await DocumentViewer.InvokeScriptAsync("eval", new string[] { functionString });
         }
     }
